e26f6e683f28d1b403792156cca3f7a3dc4e6d5d
shaon <shaon20aust@gmail.com>
2022-02-10 21:50:29 +0100

    Player movement and bakground color

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import pygame
 
 pygame.init()
-
+# create screen
 screen = pygame.display.set_mode((800, 600))
 
+# caption
 pygame.display.set_caption("Space")
-icon = pygame.image.load("")
-pygame.display.set_icon(icon)
+# icon = pygame.image.load("")
+# pygame.display.set_icon(icon)
 
+# player
+playerImg = pygame.image.load("space.jpg")
+playerX = 70
+playerY = 80
+playerX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+# game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((0, 150, 0))
+    playerX += 0.2
+    playerY -= 0.2
+    print(playerX)
+    player(playerX, playerY)
+    pygame.display.update()
